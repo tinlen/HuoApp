bcff094d3037af8e38ef2d24ac652decba0b5a27
qinqianlun <qql@@huosdk.com>
2017-11-03 15:08:30 +0800

    PermissionsDispatcher

--- app/src/main/java/com/example/huoapp/ui/MainActivity.java
+++ app/src/main/java/com/example/huoapp/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.huoapp.ui;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.KeyEvent;
 import android.widget.Toast;
 
@@ -8,9 +11,19 @@ import com.example.huoapp.R;
 import com.example.huoapp.base.baseActivity.BaseActivity;
 import com.example.huoapp.manager.AppManager;
 import com.lzy.okgo.OkGo;
+import com.socks.library.KLog;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends BaseActivity {
     private static long DOUBLE_CLICK_TIME = 0L;
+    private int position = 0;
 
     @Override
     protected int getContentViewLayoutId() {
@@ -19,7 +32,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initViewsAndEvents(Bundle savedInstanceState) {
+        MainActivityPermissionsDispatcher.getPermissionWithPermissionCheck(this);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this,requestCode,grantResults);
     }
 
     @Override
@@ -37,5 +56,37 @@ public class MainActivity extends BaseActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    //获取多个权限
+    @NeedsPermission({Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.CAMERA})
+    void getPermission(){
+        KLog.i("Has get Write external storage!");
+    }
+
+    //向用户说明为什么需要这些权限（可选）
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(final PermissionRequest request){
+        KLog.i("showRationaleForCamera");
+    }
+
+    //用户拒绝授权回调（可选）
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void showDeniedForCamera(){
+        KLog.i("OnPermissionDenied");
+    }
+
+    //用户勾选了“不再提醒”时调用（可选）
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void showNeverAskForCamera(){
+        KLog.i("OnNeverAskAgain");
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (null != intent){
+            position = intent.getIntExtra("position",0);
+        }
     }
 }
---
+++ app/src/main/java/com/example/huoapp/widget/HuoViewPager.java
@@ -0,0 +1,64 @@
+package com.example.huoapp.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by liuhongliang on 2017/8/28.
+ * 可设置是否滑动的ViewPager
+ * 1.viewpager 主要是通过重写了onInterceptTouchEvent和onTouchEvent来实现的效果，所以对这两个方法进行判断屏蔽滑动
+ * 2.当viewpager 作为子view，父view根据子view是否可以滑动来判断是否要把事件交给子view处理，所以把横向滑动进行判断是否屏蔽滑动
+ */
+
+public class HuoViewPager extends ViewPager {
+    private boolean canScroll = false;
+
+    public HuoViewPager(Context context) {
+        super(context);
+    }
+
+    public HuoViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if(this.canScroll) {
+            try {
+                return super.onInterceptTouchEvent(ev);
+            } catch (IllegalArgumentException var3) {
+                var3.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        return this.canScroll?super.onTouchEvent(event):false;
+    }
+
+    public void toggleLock() {
+        this.canScroll = !this.canScroll;
+    }
+
+    public void setCanScroll(boolean canScroll) {
+        this.canScroll = canScroll;
+    }
+
+    public boolean isCanScroll() {
+        return this.canScroll;
+    }
+
+    /**
+     * 当viewpager 作为子view，父view根据子view是否可以滑动来判断是否要把事件交给子view处理，所以把横向滑动进行判断是否屏蔽滑动
+     * @param direction
+     * @return
+     */
+    @Override
+    public boolean canScrollHorizontally(int direction) {
+        return this.canScroll?super.canScrollHorizontally(direction):false;
+    }
+}
