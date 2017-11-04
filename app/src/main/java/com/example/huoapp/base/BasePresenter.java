ece9eeda546ad153805efd54d1ca61b93c128e68
qinqianlun <qql@@huosdk.com>
2017-11-04 09:44:05 +0800

    remove http

--- app/src/main/java/com/example/huoapp/base/BasePresenter.java
+++ app/src/main/java/com/example/huoapp/base/BasePresenter.java
@@ -1,10 +1,21 @@
 package com.example.huoapp.base;
 
 /**
- * Created by tinle on 2017/11/3.
+ * Created by tinle on 2017/11/2.
  */
 
-public abstract class BasePresenter {
+public abstract class BasePresenter<V extends BaseView> {
+    private V view;
 
+    public BasePresenter(final V view){
+        this.view = view;
+    }
 
+    public V getView(){
+        return view;
+    }
+
+    public abstract void start();
+
+    public abstract void cancel();
 }
