6eb4c2713e2b4e357674219c8db7cbc9d654f31a
qinqianlun <qql@@huosdk.com>
2017-11-10 18:38:30 +0800

    head scale image view

--- app/src/main/java/com/example/huoapp/widget/ClipZoomImageView.java
+++ app/src/main/java/com/example/huoapp/widget/ClipZoomImageView.java
@@ -2,8 +2,10 @@ package com.example.huoapp.widget;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Matrix;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewTreeObserver;
@@ -21,16 +23,29 @@ public class ClipZoomImageView extends ImageView implements
     private static final String TAG = ClipZoomImageView.class.getSimpleName();
     private final float SCALE_MAX = 4.0f;
     private final float SCALE_MID = 2.0f;
+
+    /**缩放的手势检测*/
+    private ScaleGestureDetector mScaleGestureDetector = null;
+    private final Matrix mScaleMatrix = new Matrix();
+
+    /**用于双击检测*/
+    private GestureDetector mGestureDetector;
+    private boolean isAutoScale;
+
+
     public ClipZoomImageView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public ClipZoomImageView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,0);
     }
 
     public ClipZoomImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        setScaleType(ScaleType.MATRIX);
+
     }
 
     @Override
@@ -56,5 +71,13 @@ public class ClipZoomImageView extends ImageView implements
     @Override
     public void onGlobalLayout() {
 
+    }
+
+    /**
+     * 获得当前的缩放比例
+     * @return
+     */
+    public final float getScale(){
+        return 0;
     }
 }
